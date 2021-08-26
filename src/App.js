c5d9a87f027a6a08d059ed9c0e7f57d8dbc7b11d
Angel Ricardo Estrada Sevilla <e-angestrada@gentera.com.mx>
2021-08-25 20:05:23 -0500

    feat(app): Create react app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App({ saludo, children }) {
   return (
     <div className="App">
       <header className="App-header">
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React {saludo}
+          {children}
         </a>
       </header>
     </div>
