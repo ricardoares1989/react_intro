e87301e4072fde250999064435842f0852e134ae
Ricardo Estrada <angel.sevilla@eon.com.mx>
2021-08-30 21:49:22 -0500

    feat(componentes)

--- src/components/TodoSearch.js
+++ src/components/TodoSearch.js
@@ -2,8 +2,15 @@ import React from "react";
 import './TodoSearch.css'
 
 function TodoSearch(){
+  const onSearchValueChange = (event) =>{
+    console.log(event)
+  };
   return(
-    <input className="TodoSearch" placeholder="Cebolla" />
+    <input
+      className="TodoSearch"
+      placeholder="Cebolla"
+      onChange={onSearchValueChange}
+    />
   );
 }
 
